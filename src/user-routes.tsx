d7a425837eeef8737168c06b3c3d0c9907efcd94
Smrithi <smritzz0007@gmail.com>
2025-06-26 23:55:49 +0530

    Update user-routes.tsx

--- src/user-routes.tsx
+++ src/user-routes.tsx
@@ -18,7 +18,7 @@ export const userRoutes: RouteObject[] = [
 	{ path: "/auth/redirect", element: <LoginRedirect />},
 	{ path: "/auth/*", element: <StackHandlerRoutes />},
 	{ path: "/", element: <App />},
-	{ path: "/legal-search", element: <LegalSearch /></UserGuard>},
-	{ path: "/legalsearch", element:<LegalSearch /></UserGuard>},
+	{ path: "/legal-search", element: <LegalSearch />},
+	{ path: "/legalsearch", element:<LegalSearch />},
 
 ];
